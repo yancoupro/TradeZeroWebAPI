0572e9c9f6fe3fb443d0d6937b378b4b76685d5a
yannick coulombe <yannick@desoffres.com>
2024-06-13 13:03:12 -0400

    refactor::v0.1.6 import Chrome only from igos_webdriver

--- tradezeroapi/__init__.py
+++ tradezeroapi/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
+from .igos_webdriver import Chrome
 from .main import TradeZero
 from .enums import OrderType, TIF, Order, PortfolioTab
-import igos_webdriver
--- tradezeroapi/main.py
+++ tradezeroapi/main.py
@@ -8,7 +8,6 @@ from collections import namedtuple
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from igos_webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
